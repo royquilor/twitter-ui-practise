3d0cdb3120435c58318e30f1951326b4b36fdd19
royquilor <royquilor@gmail.com>
2023-01-13 13:53:57 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
   images: {
    domains: ["pbs.twimg.com"]
   }
-};
+}
 
 module.exports = nextConfig
